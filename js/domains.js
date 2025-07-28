6f0fbfa0377f8b3a5e69f46b9aa73859460ce2c0
zuixindizhi001 <frankmeng1982@gmail.com>
2025-07-28 15:21:42 +0400

    Add files via upload

---
+++ js/domains.js
@@ -0,0 +1,15 @@
+// js/domains.js
+
+const domainList = [
+    "https://1tmr7o8xxseu.top",
+    "https://ubt6blgjwdec.top",
+    "https://vcq00dso8qke.top",
+    "https://4fm4lgj6we0u.top",
+    "https://afieudlvrni3.top",
+    "https://l14s5iy9skas.top",
+    "https://oefx61iqgl30.top",
+    "https://plpidx009cxe.top",
+    "https://y62xghpxbxiz.top",
+    "https://iy132aiuv1qg.top"
+  // 确保这里正好有 10 个域名
+];
