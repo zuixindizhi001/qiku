15dbeceeae68e90c69975c0314b1ed2841414846
zuixindizhi001 <frankmeng1982@gmail.com>
2026-02-09 21:20:24 +0800

    Update domains.js

--- js/domains.js
+++ js/domains.js
@@ -1,15 +1,16 @@
 // js/domains.js
 
 const domainList = [
-    "https://1tmr7o8xxseu.top",
-    "https://ubt6blgjwdec.top",
-    "https://vcq00dso8qke.top",
-    "https://4fm4lgj6we0u.top",
-    "https://afieudlvrni3.top",
-    "https://l14s5iy9skas.top",
-    "https://oefx61iqgl30.top",
-    "https://plpidx009cxe.top",
-    "https://y62xghpxbxiz.top",
-    "https://iy132aiuv1qg.top"
+    "https://www.yxxq20.cc",
+    "https://www.yxxq21.cc",
+    "https://www.yxxq22.cc",
+    "https://www.yxxq23.cc",
+    "https://www.yxxq24.cc",
+    "https://www.yxxq25.cc",
+    "https://www.yxxq26.cc",
+    "https://www.yxxq27.cc",
+    "https://www.yxxq28.cc",
+    "https://www.yxxq29.cc",
+    "https://www.yxxq30.cc",
   // 确保这里正好有 10 个域名
 ];
